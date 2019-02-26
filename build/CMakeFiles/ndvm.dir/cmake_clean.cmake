3d2e9491e108c288bb7d5d00d229fd59a909add9
Connor Davis <davisc@ainfosec.com>
2019-02-26 08:35:58 -0700

    Remove build dir

--- build/CMakeFiles/ndvm.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ndvm"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ndvm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
