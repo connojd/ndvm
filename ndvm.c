e257ebbc7a0f39c680b21a210055e274161d8c49
Connor Davis <davisc@ainfosec.com>
2019-03-19 11:15:22 -0600

    Use select for connection polling

--- ndvm.c
+++ ndvm.c
@@ -10,6 +10,8 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
+#include <sys/select.h>
 
 /**
  * See man netdevice for info on low-level net APIs
@@ -17,125 +19,194 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 
-#define PROT_TCP 6
 #define PAGE_SIZE 4096
+#define PORT 80
 
-#define IFNAME "eth0"
-#define SERV_IP "10.1.10.22"
-#define NDVM_IP "10.1.10.191"
-#define NDVM_BRD "10.1.10.255"
-#define NDVM_MASK "255.255.255.0"
-#define NDVM_PORT 0xBF00
+/**
+ * IP address map:
+ *
+ * High-side server has highest address, then decreases
+ * by one from high-ndvm -> low-ndvm -> low-client:
+ *
+ * 192.168.191.103
+ * 192.168.191.102
+ * 192.168.191.101
+ * 192.168.191.100
+ */
+
+#define BRD "192.168.191.255"
+#define MSK "255.255.255.0"
+
+#define HI_ADDR "192.168.191.102"
+#define LO_ADDR "192.168.191.101"
 
 #define __enum_domain_op 0xBF5C000000000100
 #define __enum_domain_op__ndvm_share_page 0x143
 
-int fd;
-char *shm;
-struct ifreq ifr;
-struct sockaddr_in serv_addr;
-struct sockaddr_in *ndvm_addr;
+#define CLIENT_COUNT 2
+int client_socks[CLIENT_COUNT];
+
+/* Root listening socket */
+int nic_fd;
+fd_set read_fds;
 
 uint64_t _vmcall(uint64_t r1, uint64_t r2, uint64_t r3, uint64_t r4);
 
-static void init_ndvm(void)
+/**
+ * Initializes the network device and initializes the global nic_fd
+ * for later manipulation
+ */
+static void init_nic(char *name, char *ip)
 {
-    fd = socket(AF_INET, SOCK_STREAM, PROT_TCP);
+    int addrlen;
+    struct ifreq ifr;
+    struct sockaddr_in *addr, bind_addr;
+
+    nic_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (nic_fd == -1) {
+        exit(0xBF05);
+    }
 
     // Set name
     ifr.ifr_addr.sa_family = AF_INET;
-    memcpy(ifr.ifr_name, IFNAME, IFNAMSIZ - 1);
+    memcpy(ifr.ifr_name, name, IFNAMSIZ - 1);
 
     // Set IP address
-    ndvm_addr = (struct sockaddr_in *)&ifr.ifr_addr;
-    inet_pton(AF_INET, NDVM_IP, &ndvm_addr->sin_addr);
-    if (ioctl(fd, SIOCSIFADDR, &ifr)) {
-        printf("IP set failed: %s", strerror(errno));
-        exit(errno);
+    addr = (struct sockaddr_in *)&ifr.ifr_addr;
+    inet_pton(AF_INET, ip, &addr->sin_addr);
+    if (ioctl(nic_fd, SIOCSIFADDR, &ifr)) {
+        exit(0xBF02);
     }
 
     // Set broadcast address
-    ndvm_addr = (struct sockaddr_in *)&ifr.ifr_broadaddr;
-    inet_pton(AF_INET, NDVM_BRD, &ndvm_addr->sin_addr);
-    if (ioctl(fd, SIOCSIFBRDADDR, &ifr)) {
-        printf("BRD set failed: %s", strerror(errno));
-        exit(errno);
+    addr = (struct sockaddr_in *)&ifr.ifr_broadaddr;
+    inet_pton(AF_INET, BRD, &addr->sin_addr);
+    if (ioctl(nic_fd, SIOCSIFBRDADDR, &ifr)) {
+        exit(0xBF03);
     }
 
     // Set netmask
-    ndvm_addr = (struct sockaddr_in *)&ifr.ifr_netmask;
-    inet_pton(AF_INET, NDVM_MASK, &ndvm_addr->sin_addr);
-    if (ioctl(fd, SIOCSIFNETMASK, &ifr)) {
-        printf("MASK set failed: %s", strerror(errno));
-        exit(errno);
+    addr = (struct sockaddr_in *)&ifr.ifr_netmask;
+    inet_pton(AF_INET, MSK, &addr->sin_addr);
+    if (ioctl(nic_fd, SIOCSIFNETMASK, &ifr)) {
+        exit(0xBF03);
     }
 
     // Bring it up
     ifr.ifr_flags = IFF_UP;
-    if (ioctl(fd, SIOCSIFFLAGS, &ifr)) {
-        printf("IFF UP failed: %s", strerror(errno));
-        exit(errno);
+    if (ioctl(nic_fd, SIOCSIFFLAGS, &ifr)) {
+        exit(0xBF04);
     }
 }
 
-static void init_serv(void)
+//static inline void share_page(void *page)
+//{
+//    _vmcall(__enum_domain_op,
+//            __enum_domain_op__ndvm_share_page,
+//            (uint64_t)page,
+//            0);
+//}
+//
+//static void init_chan(void)
+//{
+//    shm = aligned_alloc(PAGE_SIZE, PAGE_SIZE);
+//    if (!shm) {
+//        exit(1);
+//    }
+//
+//    memset(shm, 0, PAGE_SIZE);
+//
+//    // Hypercall it down. the phys page this maps
+//    // to will be what we remap bfexec to
+//    share_page(shm);
+//}
+
+/**
+ * Params:
+ *      argv[1]: name of the interface
+ *      argv[2]: IPv4 of the interface
+ *
+ *      Netmask/broadcast addrs are the same for any NDVM
+ *      Each NIC has a server and a client
+ */
+
+void dump_sock(int fd, struct sockaddr_in *sa)
 {
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(NDVM_PORT);
-
-    if (inet_pton(AF_INET, SERV_IP, &serv_addr.sin_addr) <= 0) {
-        printf("init_serv: inet_pton failed: %s", strerror(errno));
-        exit(errno);
-    }
-}
-
-static inline void share_page(void *page)
-{
-    _vmcall(__enum_domain_op,
-            __enum_domain_op__ndvm_share_page,
-            (uint64_t)page,
-            0);
-}
-
-static void init_chan(void)
-{
-    shm = aligned_alloc(PAGE_SIZE, PAGE_SIZE);
-    if (!shm) {
-        exit(1);
-    }
-
-    memset(shm, 0, PAGE_SIZE);
-
-    // Hypercall it down. the phys page this maps
-    // to will be what we remap bfexec to
-    share_page(shm);
+    printf("New connection - fd: %d ip: %s port: %d\n",
+           fd,
+           inet_ntoa(sa->sin_addr),
+           ntohs(sa->sin_port));
 }
 
 int main(int argc, char **argv)
 {
-    int in;
-    char data[PAGE_SIZE];
+    struct sockaddr_in nic_sa;
+    int nic_sa_len = sizeof(nic_sa);
 
-    init_ndvm();
-    init_serv();
-    init_chan();
-
-    if (connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) {
-        printf("connect failed: %s", strerror(errno));
-        exit(errno);
+    if (argc != 3) {
+        return 0xBF01;
     }
 
-    do {
-        memset(data, 0 , PAGE_SIZE);
-        in = read(fd, data, PAGE_SIZE - 1);
-        if (in > 0) {
-            snprintf(shm + 2, in, "%s", data);
-            __asm volatile("mfence");
-            *shm = 1; // tell dom0 data is ready
-            __asm volatile("mfence");
-            while (*shm == 1) { // spin until dom0 starts to read data
-                sleep(1);
+    init_nic(argv[1], argv[2]);
+
+    memset(&nic_sa, 0, nic_sa_len);
+    nic_sa.sin_family = AF_INET;
+    nic_sa.sin_port = htons(PORT);
+    nic_sa.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(nic_fd, (struct sockaddr *)&nic_sa, sizeof(nic_sa)) < 0) {
+        exit(0xBF02);
+    }
+
+    if (listen(nic_fd, CLIENT_COUNT) < 0) {
+        exit(0xBF03);
+    }
+
+    for (int i = 0; i < CLIENT_COUNT; i++) {
+        client_socks[i] = -1;
+    }
+
+    // Wait for connections
+    while (1) {
+        int new_fd = -1;
+        int max_fd = nic_fd;
+
+        FD_ZERO(&read_fds);
+        FD_SET(nic_fd, &read_fds);
+
+        for (int i = 0; i < CLIENT_COUNT; i++) {
+            int fd = client_socks[i];
+            if (fd > 0) {
+                FD_SET(fd, &read_fds);
+                if (fd > max_fd) {
+                    max_fd = fd;
+                }
             }
         }
-    } while (in > 0);
+
+        int count = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+        if (count < 0) {
+            exit(0xBF04);
+        }
+
+        if (FD_ISSET(nic_fd, &read_fds)) {
+            new_fd = accept(nic_fd, (struct sockaddr *)&nic_sa, &nic_sa_len);
+            if (new_fd < 0) {
+                exit(0xBF05);
+            }
+        }
+
+        dump_sock(new_fd, &nic_sa);
+        char *msg = "hello\n";
+        if (send(new_fd, msg, strlen(msg), 0) != strlen(msg)) {
+            exit(0xBF06);
+        }
+
+        for (int i = 0; i < CLIENT_COUNT; i++) {
+            if (client_socks[i] == -1) {
+                client_socks[i] = new_fd;
+                break;
+            }
+        }
+    }
 }
